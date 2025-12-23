6453658bfa31bc3fdfe52df2cc5030e32907fac0
AmanUllah-99 <amankacho0001@gmail.com>
2025-12-23 19:24:13 +0500

    add new file

---
+++ 10_OOP/oop1.js
@@ -0,0 +1,41 @@
+/////Object Literal /////
+const user = {
+    userName:"Amaan",
+    loginCount : 8,
+    email : "example@gimail.com",
+    getUserDetails : function(){
+        // console.log("got users Details From DataBase");
+        // console.log(`userName: ${this.userName }`);
+        // console.log(this);
+
+        
+    }
+}
+// console.log(user.userName);
+// console.log(user.getUserDetails());
+// console.log(this);
+
+//////// Constructor Function////
+
+function User (userName , loginCount, isLoggedIn){
+    this.userName = userName,
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    
+    this.greeting = function(){
+        console.log(`welcome ${this.userName}`);
+        
+    }
+    
+
+    return this
+}
+
+
+const userOne = new  User("Amaan",12 ,true)
+const userTwo = new  User ("Zshn" , 88, false)
+
+console.log(userOne);
+console.log(userTwo);
+
